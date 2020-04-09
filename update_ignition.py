69799d06ff85ccd007839513de1ba099f992504b
dcdh <damien.clementdhuart@gmail.com>
2020-04-10 00:32:35 +0200

    add container registry configuration

--- update_ignition.py
+++ update_ignition.py
@@ -25,32 +25,29 @@ if input_ign_created_file is None:
 if input_additional_configuration_file is None:
   raise TypeError("Missing additional configuration file")
 
-with open(input_additional_configuration_file, "r") as data_additional_configuration:
+with open(input_ign_created_file, "r") as data_ign_created, open(input_additional_configuration_file, "r") as data_additional_configuration:
+  data_to_merge_to = json.load(data_ign_created)
+  data_to_merge_from = json.load(data_additional_configuration)
 
+  if hasattr(data_to_merge_to, "passwd") is False:
+    data_to_merge_to["passwd"] = {}
+    data_to_merge_to["passwd"]["users"] = []
+  if hasattr(data_to_merge_to["passwd"], "users") is False:
+    data_to_merge_to["passwd"]["users"] = []
 
-TODO merge !!!
+  data_to_merge_to["passwd"]["users"].append(data_to_merge_from["passwd"]["users"])
 
-with open(input_ign_created_file, "r") as data_ign_created:
-  data = json.load(data_ign_created)
+  if hasattr(data_to_merge_to, "storage") is False:
+    data_to_merge_to["storage"] = {}
+    data_to_merge_to["storage"]["files"] = []
+  if hasattr(data_to_merge_to["storage"], "files") is False:
+    data_to_merge_to["storage"]["files"] = []
 
-  if hasattr(data, "passwd") is False:
-    data["passwd"] = {}
-    data["passwd"]["users"] = []
-  if hasattr(data["passwd"], "users") is False:
-    data["passwd"]["users"] = []
+  data_to_merge_to["storage"]["files"].append(data_to_merge_from["storage"]["files"])
 
-  data["passwd"]["users"].append({
-    "name": user,
-    "sshAuthorizedKeys": [
-      sshPublicKey
-    ],
-    "groups": [
-      "sudo",
-      "docker"
-    ]
-  })
-  ign_created_file.close()
+  data_ign_created.close()
+  data_additional_configuration.close()
 
-with open(input_ign_created_file, "w") as ign_created_file:
-  json.dump(data, ign_created_file)
-  ign_created_file.close()
+with open(input_ign_created_file, "w") as data_ign_created:
+  json.dump(data_to_merge_to, data_ign_created)
+  data_ign_created.close()
