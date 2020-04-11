71500ed4c445e0e181d79e07c114c00a7280e4c2
dcdh <damien.clementdhuart@gmail.com>
2020-04-11 14:12:06 +0200

    fix installation of bootstrap vm by exposing secure registry (I cannot defined an insecure one)

--- update_ignition.py
+++ update_ignition.py
@@ -15,12 +15,4 @@ def merge_okd_ignition_with_additional_ignition(data_okd_ignition: json, data_ad
       data_okd_ignition['passwd']['users'] = []
     data_okd_ignition['passwd']['users'].extend(data_additional_ignition['passwd']['users'])
 
-  if 'storage' in data_additional_ignition and 'files' in data_additional_ignition['storage']:
-    if 'storage' not in data_okd_ignition:
-      data_okd_ignition['storage'] = {}
-      data_okd_ignition['storage']['files'] = []
-    if 'files' not in data_okd_ignition['storage']:
-      data_okd_ignition['storage']['files'] = []
-    data_okd_ignition['storage']['files'].extend(data_additional_ignition['storage']['files'])
-
   return data_okd_ignition
